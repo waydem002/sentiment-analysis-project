72752595e8a530ab2361674f27b122fb49fcb737
Waheed Kehinde <waydem002@yahoo.com>
2026-04-20 19:20:41 +0200

    docs: Update README with new function info

--- src/train.py
+++ src/train.py
@@ -4,6 +4,7 @@ from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.model_selection import train_test_split
 from joblib import dump
 import pandas as pd
+import argparse
 import os
 def load_and_validate_data(data_path: str) -> pd.DataFrame:
     """
@@ -15,8 +16,12 @@ def load_and_validate_data(data_path: str) -> pd.DataFrame:
     return df
 
 if __name__ == "__main__":
-    df = load_and_validate_data("sentiments.csv")
-    print(df.head())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data", default="data/sentiments.csv")
+    parser.add_argument("--out", default="models/sentiment.joblib")
+
+    args: argparse.Namespace = parser.parse_args()
+    main(data_path=args.data, model_path=args.out)
 
 def split_data(
     df: pd.DataFrame,
@@ -69,3 +74,5 @@ def main(data_path: str, model_path: str) -> None:
     print(f"Test accuracy: {acc:.3f}")
 
     save_model(clf, model_path)    
+
+    #
